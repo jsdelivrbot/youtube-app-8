35c6cb06ebcc9d90844671990e50fc065a58f72c
Bulat <bulawake@gmail.com>
2017-11-28 20:52:32 +0800

    search form state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/searchbar';
 //Create a new component. THis component should produce some html
 
+const API_KEY = 'AIzaSyA7dLuVrpqvDLP-9YmGyGd1Gc83vZ6q2bE';
+
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+		);
 };
 //Put generated HTML into the DOM
 //
