cf4fad679b4654f1f66417144562da80109da740
Bulat <bulawake@gmail.com>
2017-11-30 12:25:20 +0800

    fixed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/searchbar';
+import VideoList from './components/video_list';
 //Create a new component. THis component should produce some html
 
 const API_KEY = 'AIzaSyA7dLuVrpqvDLP-9YmGyGd1Gc83vZ6q2bE';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-		);
-};
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: []
+		};
+
+		YTSearch({key: API_KEY, term: 'world of tanks'}, videos => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return ( 
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+			)
+	}
+}
 //Put generated HTML into the DOM
 //
 ReactDOM.render(<App />, document.querySelector('.container'));
