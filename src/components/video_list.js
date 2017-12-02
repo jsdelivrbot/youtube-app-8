f2a358990c297514d1e7b3a69c58027d223dfc35
Bulat <bulawake@gmail.com>
2017-12-02 17:32:58 +0800

    origin master

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,11 +4,11 @@ import VideoListItem from './video_list_item';
 const VideoList = (props) => {
 	
 	const videoItems = props.videos.map((video)=>{
-		return <VideoListItem video={video} />
+		return <VideoListItem video={video} key={video.etag}/>
 	})
 
 	return (
-		<ul className="col-md-4 list-group">
+		<ul className="col-md-12 list-group">
 			{videoItems}
 		</ul>
 	);
